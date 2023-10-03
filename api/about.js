0118db5b3ccdb91b4412d0776723b4dd1fd98028
Shivam Manchanda <shivammanchanda@Shivams-MacBook-Pro-2.local>
2023-10-03 06:51:02 -0400

    Final Changes

--- api/about.js
+++ api/about.js
@@ -1,20 +1,3 @@
-const express = require("express");
-const app = express();
-const path = require("path");
-
-// Setting up the view engine as EJS
-app.set("view engine", "ejs");
-// Setting the views directory
-app.set("views", path.join(__dirname, "..", "views"));
-
-// Serving static files from the public directory
-app.use(express.static(path.join(__dirname, "..", "public")));
-
-app.get("/about", (req, res) => {
-  res.render("about");
-});
-
-const listener = app.listen(0, () => {
-  const { port } = listener.address();
-  process.send({ port });
-});
+module.exports = (req, res) => {
+  res.status(200).send("About page");
+};
