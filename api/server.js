3fea814b0040edb241e3234902c9feaab1bee7cb
Shivam Manchanda <shivammanchanda@Shivams-MacBook-Pro-2.local>
2023-10-03 06:37:33 -0400

    changed views path correctly

--- api/server.js
+++ api/server.js
@@ -5,10 +5,10 @@ const path = require("path");
 // Setting up the view engine as EJS
 app.set("view engine", "ejs");
 // Setting the views directory
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "..", "views"));
 
 // Serving static files from the public directory
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 app.get("/", (req, res) => {
   res.render("home");
